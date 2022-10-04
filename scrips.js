efd4efbd55684f9f410bd231f9f69722b6cd41e3
MasoudSRad <amir.sepehr72@gmail.com>
2022-10-04 18:04:27 +0330

    difficultyHandler added

--- scrips.js
+++ scrips.js
@@ -1,8 +1,10 @@
 const container = document.querySelector(".container");
 container.addEventListener("click", selectHandler);
 document.getElementById("newgame").addEventListener("click",newGame)
+document.querySelector(".difficulty").addEventListener("click", difficultyHandler)
 
 let life = 3;
+let difficulty = "easy"
 
 const randomGenerator = () => {
 	return Math.round(Math.random() * 255);
@@ -29,18 +31,26 @@ function selectHandler(event) {
 	const answer = document.querySelector(".question").innerText;
 	if (selectedColor === answer) {
 		alert("you win");
+        newGame()
 	}
     else{
         life--
         document.getElementById("life").innerText=life
         if (life === 0) {
             alert("you lose")
+            newGame()
         }
     }
 }
 
 
 function newGame(){
+    if (difficulty === "easy") {
+        life=3
+    }else if(difficulty === "hard"){
+        life=2
+    }
+    document.getElementById("life").innerText=life
     container.innerHTML=""
     for (let i = 0; i < 6; i++) {
         const box = boxGenerator();
@@ -50,3 +60,18 @@ function newGame(){
         container.append(box.box);
     }
 }
+
+function difficultyHandler(event){
+    if (event.target.innerText === "Easy") {
+        difficulty="easy"
+        document.getElementById("easy").classList.add("active")
+        document.getElementById("hard").classList.remove("active")
+    }else if(event.target.innerText === "Hard"){
+        difficulty="hard"
+        document.getElementById("hard").classList.add("active")
+        document.getElementById("easy").classList.remove("active")
+    }
+    newGame()
+}
+
+newGame()
