2306f9841ad971aa9a5695614a5ff0773ea395f0
MasoudSRad <amir.sepehr72@gmail.com>
2022-10-04 18:16:50 +0330

    2x3 amd 3x3 game mode select added

--- scrips.js
+++ scrips.js
@@ -1,10 +1,12 @@
 const container = document.querySelector(".container");
 container.addEventListener("click", selectHandler);
-document.getElementById("newgame").addEventListener("click",newGame)
-document.querySelector(".difficulty").addEventListener("click", difficultyHandler)
+document.getElementById("newgame").addEventListener("click", newGame);
+document.querySelector(".difficulty").addEventListener("click", difficultyHandler);
+document.querySelector(".mode").addEventListener("click", modeHandler);
 
 let life = 3;
-let difficulty = "easy"
+let difficulty = "easy";
+let mode = 6;
 
 const randomGenerator = () => {
 	return Math.round(Math.random() * 255);
@@ -22,56 +24,65 @@ const boxGenerator = () => {
 	return { box, color };
 };
 
-const correct = Math.round(Math.random() * 5);
-
-
+const correct = Math.round(Math.random() * (mode-1));
 
 function selectHandler(event) {
 	const selectedColor = event.path[0].style.backgroundColor;
 	const answer = document.querySelector(".question").innerText;
 	if (selectedColor === answer) {
 		alert("you win");
-        newGame()
+		newGame();
+	} else {
+		life--;
+		document.getElementById("life").innerText = life;
+		if (life === 0) {
+			alert("you lose");
+			newGame();
+		}
 	}
-    else{
-        life--
-        document.getElementById("life").innerText=life
-        if (life === 0) {
-            alert("you lose")
-            newGame()
-        }
-    }
 }
 
-
-function newGame(){
-    if (difficulty === "easy") {
-        life=3
-    }else if(difficulty === "hard"){
-        life=2
-    }
-    document.getElementById("life").innerText=life
-    container.innerHTML=""
-    for (let i = 0; i < 6; i++) {
-        const box = boxGenerator();
-        if (i === correct) {
-            document.querySelector(".question").innerText = box.color;
-        }
-        container.append(box.box);
-    }
+function newGame() {
+	if (difficulty === "easy") {
+		life = 3;
+	} else if (difficulty === "hard") {
+		life = 2;
+	}
+	document.getElementById("life").innerText = life;
+	container.innerHTML = "";
+	for (let i = 0; i < mode; i++) {
+		const box = boxGenerator();
+		if (i === correct) {
+			document.querySelector(".question").innerText = box.color;
+		}
+		container.append(box.box);
+	}
 }
 
-function difficultyHandler(event){
-    if (event.target.innerText === "Easy") {
-        difficulty="easy"
-        document.getElementById("easy").classList.add("active")
-        document.getElementById("hard").classList.remove("active")
-    }else if(event.target.innerText === "Hard"){
-        difficulty="hard"
-        document.getElementById("hard").classList.add("active")
-        document.getElementById("easy").classList.remove("active")
+function difficultyHandler(event) {
+	if (event.target.innerText === "Easy") {
+		difficulty = "easy";
+		document.getElementById("easy").classList.add("active");
+		document.getElementById("hard").classList.remove("active");
+	} else if (event.target.innerText === "Hard") {
+		difficulty = "hard";
+		document.getElementById("hard").classList.add("active");
+		document.getElementById("easy").classList.remove("active");
+	}
+	newGame();
+}
+
+function modeHandler(event) {
+    if (event.target.innerText === "3x2") {
+        mode=6
+        document.getElementById("3x2").classList.add("active");
+		document.getElementById("3x3").classList.remove("active");
+    }else if (event.target.innerText === "3x3") {
+        mode=9
+        document.getElementById("3x3").classList.add("active");
+		document.getElementById("3x2").classList.remove("active");
     }
     newGame()
 }
 
-newGame()
+newGame();
